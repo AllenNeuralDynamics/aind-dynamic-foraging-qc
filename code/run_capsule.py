8e79fae7ff6e10e086ea8497d4d993f8de4c4f41
arielleleon <ariellel@alleninstitute.org>
2026-04-15 19:43:38 +0000

    fix: add pipeline_name to log

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -438,7 +438,8 @@ def main():
     setup_logging(
         process_name,
         acquisition_name=asset_name,
-        process_name=process_name
+        process_name=process_name,
+        pipeline_name=os.getenv("PIEPLINE_NAME","")
     )
     logging.info("Begin processing...", extra={"event_type": "stage_start"})
     results_folder = Path("../results/dynamic-foraging-qc")
